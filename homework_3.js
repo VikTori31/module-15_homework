666eb5ecdc681f13d0c9a233dfc9fb68e8070693
VikKibalina <vikkibalina@gmail.com>
2023-03-25 09:41:55 +0300

    Initial commit

---
+++ homework_3.js
@@ -0,0 +1,54 @@
+const wsUri = "wss://echo-ws-service.herokuapp.com";  
+const websocket = new WebSocket(wsUri)
+
+const output = document.getElementById("output");  
+const input = document.querySelector('.text');   
+const btnSend = document.querySelector('.j-btn-send');  
+const btnGeo = document.querySelector('.j-btn-geo');    
+
+// функция для вывода сообщений в output 
+function writeToScreen(message, position) {
+  let pre = document.createElement("p");   //создаю элемент <p>
+  pre.style.wordWrap = "break-word";       //добавили стилей элементу
+  pre.innerHTML = message;                 //пишем сообщение в элементе <p> (переменная pre)
+  output.appendChild(pre);                 //добавляем это сообщ в output с помощью метода append
+}
+
+
+  websocket.onopen = function(evt) {
+		console.log("CONNECTED");
+	};
+  websocket.onmessage = function(evt) {
+    writeToScreen(
+      '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
+    );
+  };
+  websocket.onerror = function(evt) {   
+    writeToScreen(
+      '<span style="color: red;">ERROR:</span> ' + evt.data
+    );
+  };
+
+btnSend.addEventListener('click', () => {
+  const message = input.value;   
+  writeToScreen("SENT: " + message); 
+  websocket.send(message);
+});
+
+const error = () => {
+  status.textContent = 'Невозможно получить ваше местоположение';
+}
+
+  const success = (position) => {
+  const latitude  = position.coords.latitude;  
+  const longitude = position.coords.longitude;  
+  const geoLink = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+  writeToScreen (`<a  href='${geoLink}' target='_blank'>Геолокация</a>`);
+  }
+  
+btnGeo.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    writeToScreen('Geolocation не поддерживается вашим браузером'); 
+  } else { 
+    navigator.geolocation.getCurrentPosition(success, error); 
+});
